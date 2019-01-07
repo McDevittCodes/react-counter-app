16687162cdcb81d3be37a2deb5d6799c4b5f029a
Linda Allen <lindaallenx@gmail.com>
2019-01-07 16:05:05 +0000

    Update the state
    * update Delete button onClick to pass Id of Counter
    * add prefix counter to state and Delete button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: this.props.value
+        count: this.props.counter.value
     }; 
     
     // constructor() {
@@ -23,7 +23,11 @@ class Counter extends Component {
                     className="btn btn-secondary btn-sm">
                     Increment
                 </button>
-                <button onClick={this.props.onDelete} className="btn btn-danger btn-sm m-2">Delete</button>
+                <button 
+                    onClick={() => this.props.onDelete(this.props.counter.id)} 
+                    className="btn btn-danger btn-sm m-2">
+                    Delete
+                </button>
             </div>
         );
     }
